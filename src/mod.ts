a7ca896e1eaf9ec8d5d21fc6a78a6893b2e86d56
Paulov <pargsoft@hotmail.com>
2023-05-04 23:44:43 +0100

    Added commenting and lastActions SplitUrl for timestamps

--- src/mod.ts
+++ src/mod.ts
@@ -120,7 +120,9 @@ class Mod implements IPreAkiLoadMod
                     (url: string, info: any, sessionID: string, output: string): any =>
                     {
 
-                        const serverId = url.replace("/coop/server/read/lastActions/", "");
+                        var splitUrl = url.split('/');
+                        const serverId = splitUrl[splitUrl.length-2]; // url.replace("/coop/server/read/lastActions/", "");
+                        const timeStamp = splitUrl[splitUrl.length-1]; // url.replace("/coop/server/read/lastActions/", "");
 
                         if(serverId === undefined || serverId == "")
                         {
@@ -135,14 +137,19 @@ class Mod implements IPreAkiLoadMod
                             return output; 
                         }
 
-                        if(coopMatch.LastDataReceivedByAccountId[sessionID] === undefined)
+                        // Never received anything before. So just send.
+                        if(coopMatch.LastDataReceivedByAccountId[sessionID] === undefined) {
                             coopMatch.LastDataReceivedByAccountId[sessionID] = Date.now();
+                            const dataResult = coopMatch.LastDataByAccountId;
+                            output = JSON.stringify(dataResult);
+                            console.log(output.length);
+                            return output;
+                        }
 
                         const dataResult = coopMatch.LastDataByAccountId;
 
-
                         output = JSON.stringify(dataResult);
-                        console.log(output.length);
+                        // console.log(output.length);
                         return output;
                     }
                 )
@@ -274,6 +281,19 @@ class Mod implements IPreAkiLoadMod
                     url: "/coop/server/update",
                     action: (url, info, sessionId, output) => {
                         if(info === undefined || info.serverId === undefined) {
+
+                            if(JSON.stringify(info).charAt(0) === '[') {
+                                for(var item of info) {
+                                    let coopMatch = this.getCoopMatch(item.serverId);
+                                    if(coopMatch === undefined)
+                                        break;
+
+                                    coopMatch.ProcessData(item, logger);
+                                }
+                                output = JSON.stringify({});
+                                return output; 
+                            }
+
                             console.error("/coop/server/update -- no info or serverId provided");
                             output = JSON.stringify({ response: "ERROR" });
                             return JSON.stringify({ response: "ERROR" });
@@ -441,6 +461,9 @@ class Mod implements IPreAkiLoadMod
             "aki"
         );
 
+        /**
+         * WIP/UNUSED FEATURE: GET FRIENDS LIST
+         */
         container.afterResolution("DialogueController", (_t, result: DialogueController) => 
         {
             // We want to replace the original method logic with something different
@@ -451,97 +474,20 @@ class Mod implements IPreAkiLoadMod
             // The modifier Always makes sure this replacement method is ALWAYS replaced
         }, {frequency: "Always"});
 
-
+        /**
+         * MUST HAVE: REPLACE HTTP REQUEST HANDLER
+         */
         container.afterResolution("AkiHttpListener", (_t, result: AkiHttpListener) => 
         {
-            // We want to replace the original method logic with something different
             result.handle = (sessionId: string, req: IncomingMessage, resp: ServerResponse) => 
             {
-                // TODO: cleanup into interface IVerbHandler
-                switch (req.method)
-                {
-                    case "GET":
-                    {
-                        const response = result.getResponse(sessionId, req, null);
-                        result.sendResponse(sessionId, req, resp, null, response);
-                        break;
-                    }
-                    case "POST":
-                    {
-                        req.on("data", (data: any) =>
-                        {
-                            if (sessionId === undefined)
-                                sessionId = "launcher";
-
-                            const requestLength = parseInt(req.headers["content-length"]);
-                                    
-                            if (!this.httpBufferHandler.putInBuffer(sessionId, data, requestLength))
-                            {
-                                resp.writeContinue();
-                            }
-                        });
-
-                        req.on("end", async () =>
-                        {
-                            if (sessionId === undefined)
-                                sessionId = "launcher";
-
-                            const data = this.httpBufferHandler.getFromBuffer(sessionId);
-                            const value = (req.headers["debug"] === "1") ? data.toString() : zlib.inflateSync(data);
-                            if (req.headers["debug"] === "1") 
-                            {
-                                console.log(value.toString());
-                            }
-                            this.httpBufferHandler.resetBuffer(sessionId);
-                            
-                            const response = result.getResponse(sessionId, req, value);
-                            result.sendResponse(sessionId, req, resp, value, response);
-                        });
-
-                        
-                        break;
-                    }
-                    case "PUT":
-                    {
-                        req.on("data", (data) =>
-                        {
-                            // receive data
-                            //if ("expect" in req.headers)
-                            {
-                                const requestLength = parseInt(req.headers["content-length"]);
-                                    
-                                if (!this.httpBufferHandler.putInBuffer(req.headers.sessionid, data, requestLength))
-                                {
-                                    resp.writeContinue();
-                                }
-                            }
-                        });
-                            
-                        req.on("end", async () =>
-                        {
-                            const data = this.httpBufferHandler.getFromBuffer(sessionId);
-                            this.httpBufferHandler.resetBuffer(sessionId);
-                            
-                            let value = zlib.inflateSync(data);
-                            if (!value)
-                            {
-                                value = data;
-                            }
-                            const response = result.getResponse(sessionId, req, value);
-                            result.sendResponse(sessionId, req, resp, value, response);
-                        });
-                        break;
-                    }
-                    default:
-                    {
-                        break;
-                    }
-                }
+                return this.sitHttpHandler(sessionId, req, resp, result);
             }
-
-            // The modifier Always makes sure this replacement method is ALWAYS replaced
         }, {frequency: "Always"});
         
+        /**
+         * MUST HAVE: REPLACE GAME CONFIG SO IP CAN BE EXTERNAL
+         */
         container.afterResolution("GameController", (_t, result: GameController) => 
         {
             // We want to replace the original method logic with something different
@@ -622,7 +568,13 @@ class Mod implements IPreAkiLoadMod
     }
 
 
-    // TODO: Hook this up
+    /**
+     * This replaces Aki's Http Handler with a much better one that can asyncronously handle large POST requests without an error
+     * @param sessionId 
+     * @param req 
+     * @param resp 
+     * @param result 
+     */
     public sitHttpHandler(sessionId: string, req: IncomingMessage, resp: ServerResponse, result: AkiHttpListener)
     {
         // TODO: cleanup into interface IVerbHandler
@@ -636,7 +588,7 @@ class Mod implements IPreAkiLoadMod
             }
             case "POST":
             {
-                req.on("data", (data: any) =>
+                req.on("data", async (data: any) =>
                 {
                     if (sessionId === undefined)
                         sessionId = "launcher";
