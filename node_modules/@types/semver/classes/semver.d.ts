a953e32bcadfc3272db0dad6e70f7a11568283b1
mihaicm93 <45673304+mihaicm93@users.noreply.github.com>
2023-12-11 06:53:21 +0100

    Workflow update

--- node_modules/@types/semver/classes/semver.d.ts
+++ node_modules/@types/semver/classes/semver.d.ts
@@ -1,4 +1,4 @@
-import semver = require('../index');
+import semver = require("../index");
 
 declare class SemVer {
     constructor(version: string | SemVer, optionsOrLoose?: boolean | semver.RangeOptions);
@@ -13,7 +13,7 @@ declare class SemVer {
     minor: number;
     patch: number;
     version: string;
-    build: ReadonlyArray<string>;
+    build: readonly string[];
     prerelease: ReadonlyArray<string | number>;
 
     /**
--- packageBuild.ts
+++ packageBuild.ts
@@ -15,7 +15,7 @@ const path = require("path");
 const { author, name:packageName, version } = require("./package.json");
 
 // Generate the name of the package, stripping out all non-alphanumeric characters in the 'author' and 'name'.
-const modName = `${author.replace(/[^a-z0-9]/gi, "")}-${packageName.replace(/[^a-z0-9]/gi, "")}-${version}`;
+const modName = "SITCoop";
 console.log(`Generated package name: ${modName}`);
 
 // Delete the old build directory and compressed package file.
